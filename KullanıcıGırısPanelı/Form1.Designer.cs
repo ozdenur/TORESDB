a9bae05f1b5b67aee42bd6ac80a77011bffe4ac6
ozdenur <121803083+ozdenur@users.noreply.github.com>
2023-03-27 13:38:07 +0300

    Add project files.

---
+++ KullanıcıGırısPanelı/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace KullanıcıGırısPanelı
+{
+    partial class LogınForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNovas = new System.Windows.Forms.Label();
+            this.lblToplantı = new System.Windows.Forms.Label();
+            this.lblRezervasyon = new System.Windows.Forms.Label();
+            this.lblUserID = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtUserPass = new System.Windows.Forms.TextBox();
+            this.chckHatırla = new System.Windows.Forms.CheckBox();
+            this.lblBenıHatırla = new System.Windows.Forms.Label();
+            this.btnSıgnIn = new System.Windows.Forms.Button();
+            this.btnExıt = new System.Windows.Forms.Button();
+            this.txtTORES = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNovas
+            // 
+            this.lblNovas.AutoSize = true;
+            this.lblNovas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblNovas.Location = new System.Drawing.Point(126, 25);
+            this.lblNovas.Name = "lblNovas";
+            this.lblNovas.Size = new System.Drawing.Size(129, 20);
+            this.lblNovas.TabIndex = 0;
+            this.lblNovas.Text = "NOVAS WORKS";
+            // 
+            // lblToplantı
+            // 
+            this.lblToplantı.AutoSize = true;
+            this.lblToplantı.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblToplantı.Location = new System.Drawing.Point(113, 62);
+            this.lblToplantı.Name = "lblToplantı";
+            this.lblToplantı.Size = new System.Drawing.Size(161, 20);
+            this.lblToplantı.TabIndex = 1;
+            this.lblToplantı.Text = "TOPLANTI ODALARI";
+            // 
+            // lblRezervasyon
+            // 
+            this.lblRezervasyon.AutoSize = true;
+            this.lblRezervasyon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblRezervasyon.Location = new System.Drawing.Point(100, 97);
+            this.lblRezervasyon.Name = "lblRezervasyon";
+            this.lblRezervasyon.Size = new System.Drawing.Size(201, 20);
+            this.lblRezervasyon.TabIndex = 2;
+            this.lblRezervasyon.Text = "REZERVASYON SİSTEMİ";
+            // 
+            // lblUserID
+            // 
+            this.lblUserID.AutoSize = true;
+            this.lblUserID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblUserID.Location = new System.Drawing.Point(27, 168);
+            this.lblUserID.Name = "lblUserID";
+            this.lblUserID.Size = new System.Drawing.Size(97, 20);
+            this.lblUserID.TabIndex = 3;
+            this.lblUserID.Text = "User Name :";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPassword.Location = new System.Drawing.Point(29, 215);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(86, 20);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Password :";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUserName.Location = new System.Drawing.Point(130, 168);
+            this.txtUserName.MaxLength = 10;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(144, 26);
+            this.txtUserName.TabIndex = 5;
+            // 
+            // txtUserPass
+            // 
+            this.txtUserPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUserPass.Location = new System.Drawing.Point(130, 212);
+            this.txtUserPass.MaxLength = 10;
+            this.txtUserPass.Name = "txtUserPass";
+            this.txtUserPass.PasswordChar = '*';
+            this.txtUserPass.Size = new System.Drawing.Size(144, 26);
+            this.txtUserPass.TabIndex = 6;
+            // 
+            // chckHatırla
+            // 
+            this.chckHatırla.AutoSize = true;
+            this.chckHatırla.Location = new System.Drawing.Point(228, 267);
+            this.chckHatırla.Name = "chckHatırla";
+            this.chckHatırla.Size = new System.Drawing.Size(15, 14);
+            this.chckHatırla.TabIndex = 7;
+            this.chckHatırla.UseVisualStyleBackColor = true;
+            // 
+            // lblBenıHatırla
+            // 
+            this.lblBenıHatırla.AutoSize = true;
+            this.lblBenıHatırla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBenıHatırla.Location = new System.Drawing.Point(249, 262);
+            this.lblBenıHatırla.Name = "lblBenıHatırla";
+            this.lblBenıHatırla.Size = new System.Drawing.Size(91, 20);
+            this.lblBenıHatırla.TabIndex = 8;
+            this.lblBenıHatırla.Text = "Beni Hatırla";
+            // 
+            // btnSıgnIn
+            // 
+            this.btnSıgnIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSıgnIn.Location = new System.Drawing.Point(180, 311);
+            this.btnSıgnIn.Name = "btnSıgnIn";
+            this.btnSıgnIn.Size = new System.Drawing.Size(86, 30);
+            this.btnSıgnIn.TabIndex = 9;
+            this.btnSıgnIn.Text = "Sign In";
+            this.btnSıgnIn.UseVisualStyleBackColor = true;
+            this.btnSıgnIn.Click += new System.EventHandler(this.btnSıgnIn_Click);
+            // 
+            // btnExıt
+            // 
+            this.btnExıt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnExıt.Location = new System.Drawing.Point(287, 311);
+            this.btnExıt.Name = "btnExıt";
+            this.btnExıt.Size = new System.Drawing.Size(86, 30);
+            this.btnExıt.TabIndex = 10;
+            this.btnExıt.Text = "Exıt";
+            this.btnExıt.UseVisualStyleBackColor = true;
+            this.btnExıt.Click += new System.EventHandler(this.btnExıt_Click);
+            // 
+            // txtTORES
+            // 
+            this.txtTORES.Location = new System.Drawing.Point(2, 3);
+            this.txtTORES.Name = "txtTORES";
+            this.txtTORES.Size = new System.Drawing.Size(498, 20);
+            this.txtTORES.TabIndex = 11;
+            this.txtTORES.Text = "TORES v1.0";
+            // 
+            // LogınForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 383);
+            this.Controls.Add(this.txtTORES);
+            this.Controls.Add(this.btnExıt);
+            this.Controls.Add(this.btnSıgnIn);
+            this.Controls.Add(this.lblBenıHatırla);
+            this.Controls.Add(this.chckHatırla);
+            this.Controls.Add(this.txtUserPass);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUserID);
+            this.Controls.Add(this.lblRezervasyon);
+            this.Controls.Add(this.lblToplantı);
+            this.Controls.Add(this.lblNovas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LogınForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logın Form";
+          
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNovas;
+        private System.Windows.Forms.Label lblToplantı;
+        private System.Windows.Forms.Label lblRezervasyon;
+        private System.Windows.Forms.Label lblUserID;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtUserPass;
+        private System.Windows.Forms.CheckBox chckHatırla;
+        private System.Windows.Forms.Label lblBenıHatırla;
+        private System.Windows.Forms.Button btnSıgnIn;
+        private System.Windows.Forms.Button btnExıt;
+        private System.Windows.Forms.TextBox txtTORES;
+    }
+}
+
---
+++ KullanıcıGırısPanelı/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KullanıcıGırısPanelı
+{
+    public partial class LogınForm : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-V05EINL;Initial Catalog=TORESDB;Integrated Security=True"); // burada SQL bağlantı kurduk.
+      
+
+        public LogınForm()
+        {
+            InitializeComponent();
+        }
+        DateTime loginTime, errorTime,logoutTime; // burada giriş/çıkı,hata günlerini tanımladık.
+        string message;
+        private void btnExıt_Click(object sender, EventArgs e)
+        {
+            LogMsg();
+            logoutTime = DateTime.Now;
+            Application.Exit();
+        }
+
+        private void btnSıgnIn_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            string username = txtUserName.Text;
+            string password = txtUserPass.Text;
+            SqlCommand cmd = new SqlCommand("Select * From datUser where UserName=@userName and UserPass=@userPass",connection); // SQL komut vererek verileri birbirlerini bağladık
+            cmd.Parameters.AddWithValue("@userName",username);
+            cmd.Parameters.AddWithValue("@userPass",password);
+            SqlDataReader dr = cmd.ExecuteReader(); // bu verileri veritabanından  oku 
+            if (dr.Read()) // degerleri okuduysan
+            {
+                loginTime=DateTime.Now; // logintime 'a giriş saaatini ve tarihini atadık..
+                //Ana panel formundan bir nesne üret
+                //anapaneli göster Show ile
+                //bunu gizle
+                MessageBox.Show("Congrats");
+
+               
+            }
+            else
+            {
+                dr.Close();
+                errorTime = DateTime.Now;
+                LogErMsg();
+               
+                MessageBox.Show("UserName or UserPass Wrong! Please try again.","Login Error",MessageBoxButtons.OK,MessageBoxIcon.Error);// hatalı girişler için mesaj kutusu oluşturduk.
+            }
+            connection.Close();
+        }
+
+       
+
+        private void LogMsg()// SQL den aldığımız komutlar ile kullanıcı isimlerini parametrelere çevirerek veri tabanına işledik.
+        {
+            connection.Open();
+            string userName = txtUserName.Text;
+            message = $"Login Message:{userName} successfully login";
+            connection.Open();
+            SqlCommand cmd2 = new SqlCommand("Insert into datLog (UserID,LoginDT,LogoutDT,LogNotes) values (@userId,@loginDt,@logoutDt,@logNotes",connection);
+            cmd2.Parameters.AddWithValue("@userId",userName);
+            cmd2.Parameters.AddWithValue("@loginDt", loginTime.ToString()) ;
+            cmd2.Parameters.AddWithValue("@logoutDt", logoutTime.ToString());
+            cmd2.Parameters.AddWithValue("@logNotes", message.ToString());
+            cmd2.ExecuteNonQuery(); // değişiklikleri veritabanına yansıt .. Kaydet gibi
+            connection.Close();
+        }
+        private void LogErMsg()
+        {
+            string userName = txtUserName.Text;
+            message = "Error Message:login error";
+            SqlCommand cmd2 = new SqlCommand("Insert into datLog (UserID,LoginDT,LogoutDT,LogNotes) values (@userId,@loginDt,@logoutDt,@logNotes)", connection);
+            cmd2.Parameters.AddWithValue("@userId", 3);
+            cmd2.Parameters.AddWithValue("@loginDt", errorTime);
+            cmd2.Parameters.AddWithValue("@logoutDt", errorTime);
+            cmd2.Parameters.AddWithValue("@logNotes", message);
+            cmd2.ExecuteNonQuery(); // değişiklikleri veritabanına yansıt .. Kaydet gibi
+        }
+    }
+}
